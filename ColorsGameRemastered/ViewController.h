cca5bb4121fea7d7322987548612634cf2c5cc99
Igor Vedeneev <igorvedeneev@Mac-mini-Igor.local>
2015-07-12 00:18:37 +0300

    Initial Commit

---
+++ ColorsGameRemastered/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ColorsGameRemastered
+//
+//  Created by Igor Vedeneev on 12.07.15.
+//  Copyright (c) 2015 Igor Vedeneev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ColorsGameRemastered/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ColorsGameRemastered
+//
+//  Created by Igor Vedeneev on 12.07.15.
+//  Copyright (c) 2015 Igor Vedeneev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
